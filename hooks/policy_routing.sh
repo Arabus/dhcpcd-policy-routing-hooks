87f1baa48836d50d4f99eb30261500868d33abd0
Bernd May <bm@dv-team.de>
2016-11-12 00:22:41 +0100

    Fix infinite loop in policy routing, push new muacc

--- hooks/policy_routing.sh
+++ hooks/policy_routing.sh
@@ -1,88 +1,98 @@
 _get_free_routing_table_id(){
-  # TODO fix this
-  printf "%i" $(($RANDOM % 253 + 1))
+  local new_id=$(/sbin/ip -6 route show table all | grep "table" | sed 's/.*\(table.*\)/\1/g' | awk '{print $2}' | sort | uniq | grep -e "[0-9]" | sort -rn | head -n1)
+  [ -z "${new_id}" ] && new_id=0
+  new_id=$(($new_id + 1))
+  printf "%i" $new_id
 }
 
 _add_or_update_policy(){
-  # for each global prefix create or replace a new source route to a routing table
-  # also add a default route to each routing table
-  local prefix_addr=$1
-  local prefix_gw=$2
-  local prefix_table=$(/sbin/ip -6 ru | grep "from ${prefix_addr}" | sed -rne 's/.*lookup ([0-9]+)/\1/p')
-  [ -z "$prefix_table" ] && \
-    local table_id=$(_get_free_routing_table_id) && \
-    /sbin/ip -6 r a table ${table_id} default via ${prefix_gw} && \
-    /sbin/ip -6 rule add from ${prefix_addr} table ${table_id}
 
-  /sbin/ip -6 r r table ${prefix_table} default via ${prefix_gw}
+  local prefix_addr=${1?"Source address required"}
+  local prefix_gw=${2?gateway address required}
+  local prefix_table=$(/sbin/ip -6 ru | grep "from ${prefix_addr}" | head -n1 | sed -rne 's/.*lookup ([0-9]+)/\1/p')
+  if [ -z "$prefix_table" ]; then
+    local table_id=$(_get_free_routing_table_id)
+    /sbin/ip -6 route add table ${table_id} default via ${prefix_gw} dev ${interface}
+    /sbin/ip -6 rule add from ${prefix_addr} table ${table_id}
+    return 0
+  fi
+
+  /sbin/ip -6 r r table ${prefix_table} default via ${prefix_gw} dev ${interface}
 }
 
 _add_policy_routing(){
         # Loop to extract the ND prefix and router options using our indexed shell values
-        local -i i=1
-        local -i j=1
+        local refix=''
+        local i=1
+        local j=1
         while true; do
                 while true; do
-                        eval prefix=$nd${i}_prefix_information${j}_prefix
+                        eval prefix=\$nd${i}_prefix_information${j}_prefix
                         [ -z "${prefix}" ] && break
-                        eval prefix_vltime=$nd${i}_prefix_information${j}_vltime
+                        eval prefix_vltime=\$nd${i}_prefix_information${j}_vltime
                         # do not setup policies for prefixes to be removed
-                        [ ${prefix_vltime} -eq 0 ] && let j++ && continue
+                        [ ${prefix_vltime} -eq 0 ] && j=$(($j + 1)) && continue
 
-                        eval prefix_addr=$nd${i}_addr${j}
-                        eval router_address=$nd${i}_from
+                        eval prefix_addr=\$nd${i}_addr${j}
+                        eval router_address=\$nd${i}_from
+                        [ -z "${prefix_addr}" ] || [ -z "${router_address}" ] && break
                         _add_or_update_policy ${prefix_addr} ${router_address}
-                        let j++
+                        j=$(($j + 1))
                 done
 
                 [ -z "${prefix}" ] && break
-                let i++
+                i=$(($i + 1))
         done
 }
-
 _remove_policy(){
   local prefix_addr=$1
   local prefix_table=$(/sbin/ip -6 ru | grep "from ${prefix_addr}" | sed -rne 's/.*lookup ([0-9]+)/\1/p')
 
   [ -z "$prefix_table" ] && return 0
-  /sbin/ip -6 rule d from ${prefix_addr} table "${prefix_table}"
-  /sbin/ip -6 r d table ${prefix_table}
+  while /sbin/ip -6 ru d from ::/0 to ::/0 table ${prefix_table} &>/dev/null; do
+    true
+  done
+  /sbin/ip -6 r f table ${prefix_table}
 }
 
-
-The contents...
-licy_routing(){
+_remove_policy_routing(){
         # Loop to extract the ND prefix and router options using our indexed shell values
-        local -i i=1
-        local -i j=1
+        local prefix=''
+        local i=1
+        local j=1
         while true; do
                 while true; do
-                        eval prefix=$nd${i}_prefix_information${j}_prefix
+                        eval prefix=\$nd${i}_prefix_information${j}_prefix
                         [ -z "${prefix}" ] && break
-                        eval prefix_vltime=$nd${i}_prefix_information${j}_vltime
+                        eval prefix_vltime=\$nd${i}_prefix_information${j}_vltime
                         # remove policies for prefixes to be removed
-                        [ ${prefix_vltime} -ne 0 ] && let j++ && continue
+                        [ ${prefix_vltime} -ne 0 ] && j=$(($j + 1)) && continue
 
-                        eval prefix_addr=$nd${i}_addr${j}
+                        eval prefix_addr=\$nd${i}_addr${j}
                         _remove_policy ${prefix_addr}
-                        let j++
+                        j=$(($j + 1))
                 done
 
                 [ -z "${prefix}" ] && break
-                let i++
+                i=$(($i + 1))
         done
 }
 
 _stop_policy_routing(){
   # remove all policy routes and tables for managed interfaces
+  # /sbin/ip -6 r d table 10
   local tables=$(ip route show table all | grep "table" | sed 's/.*\(table.*\)/\1/g' | awk '{print $2}' | sort | uniq | grep -e "[0-9]")
   for table_number in $tables; do
-    /sbin/ip -6 r d table ${table_number}
+    /sbin/ip -6 r f table ${table_number}
+    while /sbin/ip -6 ru d from ::/0 to ::/0 table ${table_number} &>/dev/null; do
+      true
+    done
   done
 }
 
 case "$reason" in
-ROUTER*)
+ROUTERADVERT)
+        set | sort >> /tmp/pdump
         _add_policy_routing
         _remove_policy_routing
         exit 0
@@ -90,3 +100,4 @@ ROUTER*)
 STOP*)
         _stop_policy_routing
 esac
+
